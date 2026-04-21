e5223516fa903837240a6de72201f4690644f1ed
Prasoon9336 <prasoonsrivastava44@gmail.com>
2026-04-21 08:27:03 +0530

    Used Switch

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -18,43 +18,69 @@ namespace Calculator
             Console.WriteLine("1 Add, 2 Sub, 3 Multi, 4 division, 5 Remainder, 6 Increment Both Values, 7 Decrement Both Values");
             int z = Convert.ToInt32(Console.ReadLine());
 
-            if (z == 1)
+            //if (z == 1)
+            //{
+            //    Console.WriteLine((x + y));
+            //    Console.ReadLine();
+            //}
+            //else if (z == 2)
+            //{
+            //    Console.WriteLine((x - y));
+            //    Console.ReadLine();
+            //}
+            //else if (z == 3)
+            //{
+            //    Console.WriteLine((x * y));
+            //    Console.ReadLine();
+            //}
+            //else if (z == 4)
+            //{
+            //    Console.WriteLine((x / y));
+            //    Console.ReadLine();
+            //}
+            //else if (z == 5)
+            //{
+            //    Console.WriteLine((x % y));
+            //    Console.ReadLine();
+            //}
+            //else if (z == 6) {
+            //    Console.WriteLine((++x , ++y));
+            //}
+            //else if (z == 7)
+            //{
+            //    Console.WriteLine((--x , --y));
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Operation, Retry Again");
+            //    goto Start;
+            //}
+            switch (z)
             {
-                Console.WriteLine((x + y));
-                Console.ReadLine();
+                case 1:
+                    Console.WriteLine((x + y));
+                    break;
+                case 2:
+                    Console.WriteLine((x - y));
+                    break;
+                case 3:
+                    Console.WriteLine((x * y));
+                    break;
+                case 4:
+                    Console.WriteLine((x / y));
+                    break;
+                case 5:
+                    Console.WriteLine((x % y));
+                    break;
+                case 6:
+                    Console.WriteLine((++x, ++y));
+                    break;
+                case 7:
+                    Console.WriteLine((--x, --y));
+                    break;
+                default:
+                    Console.WriteLine("Invalid Operation, Retry Again");
+                    goto Start;
             }
-            else if (z == 2)
-            {
-                Console.WriteLine((x - y));
-                Console.ReadLine();
-            }
-            else if (z == 3)
-            {
-                Console.WriteLine((x * y));
-                Console.ReadLine();
-            }
-            else if (z == 4)
-            {
-                Console.WriteLine((x / y));
-                Console.ReadLine();
-            }
-            else if (z == 5)
-            {
-                Console.WriteLine((x % y));
-                Console.ReadLine();
-            }
-            else if (z == 6) {
-                Console.WriteLine((++x , ++y));
-            }
-            else if (z == 7)
-            {
-                Console.WriteLine((--x , --y));
-            }
-            else
-            {
-                Console.WriteLine("Invalid Operation, Retry Again");
-                goto Start;
-            }
-        }
     }
 }
