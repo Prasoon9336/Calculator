0ba829ae38138ba71ade90f419af1d57369162b4
Prasoon9336 <prasoonsrivastava44@gmail.com>
2026-04-21 08:34:06 +0530

    Implemented 2 Ways To Operate

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -18,43 +18,53 @@ namespace Calculator
             Console.WriteLine("1 Add, 2 Sub, 3 Multi, 4 division, 5 Remainder, 6 Increment Both Values, 7 Decrement Both Values");
             int z = Convert.ToInt32(Console.ReadLine());
 
-            //if (z == 1)
-            //{
-            //    Console.WriteLine((x + y));
-            //    Console.ReadLine();
-            //}
-            //else if (z == 2)
-            //{
-            //    Console.WriteLine((x - y));
-            //    Console.ReadLine();
-            //}
-            //else if (z == 3)
-            //{
-            //    Console.WriteLine((x * y));
-            //    Console.ReadLine();
-            //}
-            //else if (z == 4)
-            //{
-            //    Console.WriteLine((x / y));
-            //    Console.ReadLine();
-            //}
-            //else if (z == 5)
-            //{
-            //    Console.WriteLine((x % y));
-            //    Console.ReadLine();
-            //}
-            //else if (z == 6) {
-            //    Console.WriteLine((++x , ++y));
-            //}
-            //else if (z == 7)
-            //{
-            //    Console.WriteLine((--x , --y));
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Invalid Operation, Retry Again");
-            //    goto Start;
-            //}
+
+            /* Two ways to calculate one comment out other is implemented to avoid confusion 
+             * and 
+             * to show both ways of doing the same thing
+             * 
+             * 
+             * 1st way is using if else statement
+             *
+             * if (z == 1)
+             * {
+             * Console.WriteLine((x + y));
+             * Console.ReadLine();
+             * }
+             * else if (z == 2)
+             * {
+             * Console.WriteLine((x - y));
+             * Console.ReadLine();
+             * }
+             * else if (z == 3)
+             * {
+             * Console.WriteLine((x * y));
+             * Console.ReadLine();
+             * }
+             * else if (z == 4)
+             * {
+             * Console.WriteLine((x / y));
+             * Console.ReadLine();
+             * }
+             * else if (z == 5)
+             * {
+             * Console.WriteLine((x % y));
+             * Console.ReadLine();
+             * }
+             * else if (z == 6)
+             * {
+             * Console.WriteLine((++x, ++y));
+             * }
+             * else if (z == 7)
+             * {
+             * Console.WriteLine((--x, --y));
+             * }
+             *
+             */
+
+
+            // 2nd way is using switch statement
+
             switch (z)
             {
                 case 1:
@@ -82,5 +92,6 @@ namespace Calculator
                     Console.WriteLine("Invalid Operation, Retry Again");
                     goto Start;
             }
+        }
     }
 }
